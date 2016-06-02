86870c0e7234f720a042b634fafd293345f2a1c2
YooYoungSun <youngsun.yoo@sundaytoz.com>
2016-06-02 21:02:03 +0900

    [수정] 유저 데이터 서버에서 받아오면 air에 이벤트 전달하도록 수정

--- Ane/KaKaoExtension.java/src/com/extensions/kakao/UserProfileFunction.java
+++ Ane/KaKaoExtension.java/src/com/extensions/kakao/UserProfileFunction.java
@@ -4,35 +4,73 @@ import com.adobe.fre.FREContext;
 import com.adobe.fre.FREFunction;
 import com.adobe.fre.FREObject;
 import com.adobe.fre.FREWrongThreadException;
+import com.kakao.auth.ErrorCode;
+import com.kakao.network.ErrorResult;
+import com.kakao.usermgmt.UserManagement;
+import com.kakao.usermgmt.callback.MeResponseCallback;
+import com.kakao.usermgmt.response.model.UserProfile;
+import com.kakao.util.helper.log.Logger;
+
+import android.util.Log;
 
 
 //현재 접속 중이 유저의 정보를 리턴 합니다.
 public class UserProfileFunction implements FREFunction{
-	private FREObject   _playerFREObject;
+
 	private String   _playerString = "";
-	
+	private static String TAG = "NativeExtension";
 	@Override
 	public FREObject call(FREContext arg0, FREObject[] arg1) {
 		// TODO Auto-generated method stub
-		_playerString = "";
-		
-		_playerString += LoginActivity.userProfile().getId();
-		_playerString += "$";
-		
-		_playerString += LoginActivity.userProfile().getNickname();
-		_playerString += "$";
-		
-		_playerString += LoginActivity.userProfile().getProfileImagePath();
-		_playerString += "$";
-		
-		try {
-			_playerFREObject = FREObject.newObject(_playerString);
-		} catch (FREWrongThreadException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+
+		requestMe();
 			
-		return _playerFREObject;
+		return null;
+	}
+	
+	public void requestMe() { //유저의 정보를 받아오는 함수
+		UserManagement.requestMe(new MeResponseCallback() {
+
+			@Override
+			public void onFailure(ErrorResult errorResult) {
+				String message = "failed to get user info. msg=" + errorResult;
+				Logger.d(message);
+
+				ErrorCode result = ErrorCode.valueOf(errorResult.getErrorCode());
+				if (result == ErrorCode.CLIENT_ERROR_CODE) {
+				} else {
+
+				}
+			}
+
+			@Override
+			public void onSessionClosed(ErrorResult errorResult) {
+			}
+
+			@Override
+			public void onNotSignedUp() {} // 카카오톡 회원이 아닐 시 showSignup(); 호출해야함
+
+			@Override
+			public void onSuccess(UserProfile userProfile) {  //성공 시 userProfile 형태로 반환
+				Log.d(TAG,"UserProfile" + userProfile.getProperty("score"));
+				_playerString = "";
+				
+				_playerString += userProfile.getId();
+				_playerString += "$";
+				
+				_playerString += userProfile.getNickname();
+				_playerString += "$";
+				
+				_playerString += userProfile.getProfileImagePath();
+				_playerString += "$";
+				
+				_playerString += userProfile.getProperty("score");
+				_playerString += "$";
+
+				Log.d(TAG,"_playerString" + _playerString);
+				InitFunction._flashActivity.dispatchStatusEventAsync(_playerString, "GET_USERDATA");
+			}
+		});
 	}
 }
 
